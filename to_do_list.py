394cffbca8e054150ea271c88fdce105c365f854
gajmerkumar <gajmerk17@gmail.com>
2024-12-11 21:18:11 +0545

    update

--- to_do_list.py
+++ to_do_list.py
@@ -1,12 +1,16 @@
 tasks = []
 
-print("Welcome to the to-do-list app :)")
-print("1. Add task")
-print("2. List task")
-print("3. Delete task")
-print("4. Update task")
-print("5. Exit")
 print("\n")
+print("Welcome to the to-do-list app :)")
+
+def welcome_message():
+    print("Select your choice")    
+    print("1. Add task")
+    print("2. List task")
+    print("3. Delete task")
+    print("4. Update task")
+    print("5. Exit")
+    print("\n")
     
 def addTask():
     print("-------------------------------------")
@@ -21,20 +25,23 @@ def listTask():
     else:
         print("\n")
         print("Your tasks:")
-        for index, value in enumerate(tasks, 1):
+        for index, value in enumerate(tasks):
             print(f"Task #{index}. {value}")
         print("\n")
         
 
 def deleteTask():
+    print("\n")
     print("Choose the task you want to delete:")
     listTask()
     task_to_delete = int(input("Enter # to delete: "))
     if task_to_delete >= 0 and task_to_delete < len(tasks):
         removed_task = tasks.pop(task_to_delete-1)
         print(f"Task '{removed_task}' deleted.")
+        print("\n")
     else:
         print("Invalid number")
+        print("\n")
     
 
 
@@ -49,6 +56,7 @@ def updateTask():
     
 
 while True:
+    welcome_message()
     user_choice = int(input("Please enter your choice: "))
     if user_choice == 1:
         addTask()
